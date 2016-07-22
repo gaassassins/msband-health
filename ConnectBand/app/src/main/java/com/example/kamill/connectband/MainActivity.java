9be434dab4ada0ebe7dc8ba5a28c578cd8abe9c7
Maria Malyarova <savoreux69@gmail.com>
2016-07-22 14:52:10 +0300

    Some change of layouts

---
+++ ConnectBand/app/src/main/java/com/example/kamill/connectband/MainActivity.java
@@ -0,0 +1,328 @@
+package com.example.kamill.connectband;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+import java.lang.ref.WeakReference;
+
+import com.microsoft.band.BandClient;
+import com.microsoft.band.BandClientManager;
+import com.microsoft.band.BandException;
+import com.microsoft.band.BandIOException;
+import com.microsoft.band.BandInfo;
+import com.microsoft.band.ConnectionState;
+import com.microsoft.band.UserConsent;
+import com.microsoft.band.sensors.BandHeartRateEvent;
+import com.microsoft.band.sensors.BandHeartRateEventListener;
+import com.microsoft.band.sensors.HeartRateConsentListener;
+import com.microsoft.band.sensors.SampleRate;
+import com.microsoft.band.sensors.BandGyroscopeEvent;
+import com.microsoft.band.sensors.BandGyroscopeEventListener;
+import com.microsoft.band.sensors.BandAccelerometerEvent;
+import com.microsoft.band.sensors.BandAccelerometerEventListener;
+
+
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity {
+    private BandClient client = null;
+    private Button btnStart, btnConsent, btnAcc, btnGyro;
+    private TextView txtStatus, txtAcc, txtGyro;
+
+    private BandHeartRateEventListener mHeartRateEventListener = new BandHeartRateEventListener() {
+        @Override
+        public void onBandHeartRateChanged(final BandHeartRateEvent event) {
+            if (event != null) {
+                appendToUI(String.format("Heart Rate = %d beats per minute\n", event.getHeartRate()));
+            }
+        }
+    };
+
+    private BandAccelerometerEventListener mAccelerometerEventListener = new BandAccelerometerEventListener() {
+        @Override
+        public void onBandAccelerometerChanged(BandAccelerometerEvent event) {
+            if (event != null){
+                appendToUI_Acc(String.format(" X = %.3f \n Y = %.3f\n Z = %.3f", event.getAccelerationX(),
+                        event.getAccelerationY(), event.getAccelerationZ()));
+            }
+        }
+    };
+
+    private BandGyroscopeEventListener mGyroscopeEventListener = new BandGyroscopeEventListener() {
+        @Override
+        public void onBandGyroscopeChanged(BandGyroscopeEvent event) {
+            if (event != null){
+                appendToUI_Gyro(String.format(" X = %.3f \n Y = %.3f\n Z = %.3f", event.getAngularVelocityX(),
+                        event.getAngularVelocityY(), event.getAngularVelocityZ()));
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtStatus = (TextView) findViewById(R.id.txtStatus);
+        txtAcc = (TextView) findViewById(R.id.txtAcc);
+        txtGyro = (TextView) findViewById(R.id.txtGyro);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtStatus.setText("");
+                new HeartRateSubscriptionTask().execute();
+            }
+        });
+
+        final WeakReference<Activity> reference = new WeakReference<Activity>(this);
+
+        btnConsent = (Button) findViewById(R.id.btnConsent);
+        btnConsent.setOnClickListener(new OnClickListener() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public void onClick(View v) {
+                new HeartRateConsentTask().execute(reference);
+            }
+        });
+
+        btnAcc = (Button) findViewById(R.id.btnAcc);
+        btnAcc.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtAcc.setText("");
+                new AccelerometerSubscriptionTask().execute();
+            }
+        });
+
+        btnGyro = (Button) findViewById(R.id.btnGyro);
+        btnGyro.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtGyro.setText("");
+                new GyroscopeSubscriptionTask().execute();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        txtStatus.setText("");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (client != null) {
+            try {
+                client.getSensorManager().unregisterAllListeners();
+            } catch (BandIOException e) {
+                appendToUI(e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (client != null) {
+            try {
+                client.disconnect().await();
+            } catch (InterruptedException e) {
+                // Do nothing as this is happening during destroy
+            } catch (BandException e) {
+                // Do nothing as this is happening during destroy
+            }
+        }
+        super.onDestroy();
+    }
+
+    private class GyroscopeSubscriptionTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (getConnectedBandClient()) {
+                    appendToUI_Gyro("Band is connected.\n");
+                    client.getSensorManager().registerGyroscopeEventListener(mGyroscopeEventListener, SampleRate.MS128);
+                } else {
+                    appendToUI_Gyro("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
+                }
+            } catch (BandException e) {
+                String exceptionMessage="";
+                switch (e.getErrorType()) {
+                    case UNSUPPORTED_SDK_VERSION_ERROR:
+                        exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
+                        break;
+                    case SERVICE_ERROR:
+                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
+                        break;
+                    default:
+                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
+                        break;
+                }
+                appendToUI_Gyro(exceptionMessage);
+
+            } catch (Exception e) {
+                appendToUI_Gyro(e.getMessage());
+            }
+            return null;
+        }
+    }
+
+    private class AccelerometerSubscriptionTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (getConnectedBandClient()) {
+                    appendToUI_Acc("Band is connected.\n");
+                    client.getSensorManager().registerAccelerometerEventListener(mAccelerometerEventListener, SampleRate.MS128);
+                } else {
+                    appendToUI_Acc("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
+                }
+            } catch (BandException e) {
+                String exceptionMessage="";
+                switch (e.getErrorType()) {
+                    case UNSUPPORTED_SDK_VERSION_ERROR:
+                        exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
+                        break;
+                    case SERVICE_ERROR:
+                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
+                        break;
+                    default:
+                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
+                        break;
+                }
+                appendToUI_Acc(exceptionMessage);
+
+            } catch (Exception e) {
+                appendToUI_Acc(e.getMessage());
+            }
+            return null;
+        }
+    }
+
+    private class HeartRateSubscriptionTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                if (getConnectedBandClient()) {
+                    if (client.getSensorManager().getCurrentHeartRateConsent() == UserConsent.GRANTED) {
+                        client.getSensorManager().registerHeartRateEventListener(mHeartRateEventListener);
+                    } else {
+                        appendToUI("You have not given this application consent to access heart rate data yet."
+                                + " Please press the Heart Rate Consent button.\n");
+                    }
+                } else {
+                    appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
+                }
+            } catch (BandException e) {
+                String exceptionMessage="";
+                switch (e.getErrorType()) {
+                    case UNSUPPORTED_SDK_VERSION_ERROR:
+                        exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
+                        break;
+                    case SERVICE_ERROR:
+                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
+                        break;
+                    default:
+                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
+                        break;
+                }
+                appendToUI(exceptionMessage);
+
+            } catch (Exception e) {
+                appendToUI(e.getMessage());
+            }
+            return null;
+        }
+    }
+
+    private class HeartRateConsentTask extends AsyncTask<WeakReference<Activity>, Void, Void> {
+        @Override
+        protected Void doInBackground(WeakReference<Activity>... params) {
+            try {
+                if (getConnectedBandClient()) {
+
+                    if (params[0].get() != null) {
+                        client.getSensorManager().requestHeartRateConsent(params[0].get(), new HeartRateConsentListener() {
+                            @Override
+                            public void userAccepted(boolean consentGiven) {
+                            }
+                        });
+                    }
+                } else {
+                    appendToUI("Band isn't connected. Please make sure bluetooth is on and the band is in range.\n");
+                }
+            } catch (BandException e) {
+                String exceptionMessage="";
+                switch (e.getErrorType()) {
+                    case UNSUPPORTED_SDK_VERSION_ERROR:
+                        exceptionMessage = "Microsoft Health BandService doesn't support your SDK Version. Please update to latest SDK.\n";
+                        break;
+                    case SERVICE_ERROR:
+                        exceptionMessage = "Microsoft Health BandService is not available. Please make sure Microsoft Health is installed and that you have the correct permissions.\n";
+                        break;
+                    default:
+                        exceptionMessage = "Unknown error occured: " + e.getMessage() + "\n";
+                        break;
+                }
+                appendToUI(exceptionMessage);
+
+            } catch (Exception e) {
+                appendToUI(e.getMessage());
+            }
+            return null;
+        }
+    }
+
+    private void appendToUI(final String string) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                txtStatus.setText(string);
+            }
+        });
+    }
+
+    private void appendToUI_Acc(final String string) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                txtAcc.setText(string);
+            }
+        });
+    }
+
+    private void appendToUI_Gyro(final String string) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                txtGyro.setText(string);
+            }
+        });
+    }
+
+    private boolean getConnectedBandClient() throws InterruptedException, BandException {
+        if (client == null) {
+            BandInfo[] devices = BandClientManager.getInstance().getPairedBands();
+            if (devices.length == 0) {
+                appendToUI("Band isn't paired with your phone.\n");
+                return false;
+            }
+            client = BandClientManager.getInstance().create(getBaseContext(), devices[0]);
+        } else if (ConnectionState.CONNECTED == client.getConnectionState()) {
+            return true;
+        }
+
+        appendToUI("Band is connecting...\n");
+        return ConnectionState.CONNECTED == client.connect().await();
+    }
+}
